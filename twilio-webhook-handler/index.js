3a040b2bef2f4c2aee73c1d10ebe75e321d5091c
edwardgoode <edward.s.goode@vanderbilt.edu>
2019-04-17 17:24:25 -0500

    add state and response storage

--- twilio-webhook-handler/index.js
+++ twilio-webhook-handler/index.js
@@ -10,15 +10,15 @@ let cachedClient = null;
 function connectToDatabase (uri) {
   console.log('=> connect to database');
 
-  if (cachedDb) {
+  if (cachedDb && cachedDb.serverConfig.isConnected()) {
     console.log('=> using cached database instance');
     return Promise.resolve(cachedDb);
   }
 
   return MongoClient.connect(uri, { useNewUrlParser: true })
     .then(client => {
-      cachedDb = client.db(dbName);
       cachedClient = client;
+      cachedDb = cachedClient.db(dbName);
       return cachedDb;
     });
 }
@@ -30,7 +30,7 @@ function queryDatabase (db, collection, queryParams = {}) {
   return db.collection(collection).find(queryParams).toArray()
     .then(res => { 
       console.log("=> query result", res);
-      return { statusCode: 200, body: JSON.stringify(res)}; 
+      return res; 
      })
     .catch(err => {
       console.log('=> an error occurred: ', err);
@@ -59,13 +59,30 @@ exports.handler = function(context, event, callback) {
   const formValues = qs.parse(context.body);
   const twiml = new MessagingResponse();
 
+  console.log(formValues);
+  var query =  { number : formValues.From };
+
   connectToDatabase(MONGODB_URI)
-    .then(db => queryDatabase(db, 'users'))
+    .then(db => queryDatabase(db, 'users', query))
     .then(result => {
       console.log('=> returning result: ', result);
+      var userDoc = result[0];
+      var curState = userDoc.state;
+      
+      if(curState < userDoc.questions.length){
+        twiml.message(JSON.stringify(userDoc.questions[curState]));
+        cachedDb.collection('users').updateOne({number : formValues.From}, {$set: { "state" : curState + 1}});
+        if(curState == 0){
+          var doc = { number : formValues.From, responses : [formValues.Body]}
+          cachedDb.collection('responses').insertOne(doc);
+        } else {
+          cachedDb.collection('responses').updateOne({number : formValues.From}, { $push: { responses: formValues.Body }});
+        }
+      }
+      
 
-      cachedClient.close();
-      twiml.message(JSON.stringify(result));
+
+      cachedClient.close()
       
       callback(null, {
 		    statusCode: 200,
